db7cd8237b12fd1113928c66774e55a2ba835864
Walter Cervini <wcervini@gmail.com>
2026-06-16 08:45:01 +0100

    fix: :bug: correct integration usage in astro.config.mjs and update eslint.config.mjs for better compatibility chore: rename package and update dependencies in package.json

--- base/astro.config.mjs
+++ base/astro.config.mjs
@@ -5,7 +5,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations:[icon]
+  integrations:[icon()],
   vite: {
     plugins: [tailwind()],
   },
--- base/eslint.config.mjs
+++ base/eslint.config.mjs
@@ -1,33 +1,33 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
-import tseslint from 'typescript-eslint';
 import tailwind from 'eslint-plugin-tailwindcss';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  // Configuración base recomendada por ESLint y TypeScript
+export default [
+  // Configuracion base recomendada por ESLint y TypeScript
   ...tseslint.configs.recommended,
 
-  // Configuración recomendada para archivos Astro
+  // Configuracion recomendada para archivos Astro
   ...eslintPluginAstro.configs.recommended,
 
-  // Configuración para validar clases de Tailwind CSS
+  // Configuracion para validar clases de Tailwind CSS
   ...tailwind.configs['flat/recommended'],
 
   // Reglas personalizadas y anulaciones
   {
-    files: ['**/*.astro', '**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    files: ['/*.astro', '/.ts', '/.tsx', '/*.js', '/*.jsx'],
     rules: {
       // Evita conflictos menores entre las etiquetas de Astro y TypeScript
-      '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
 
-      // Reglas específicas para optimizar el uso de Tailwind CSS
+      // Reglas especificas para optimizar el uso de Tailwind CSS
       'tailwindcss/classnames-order': 'warn',
       'tailwindcss/no-custom-classname': 'off',
     },
   },
 
-  // Tratamiento específico para el Frontmatter de los archivos Astro
+  // Tratamiento especifico para el Frontmatter de los archivos Astro
   {
-    files: ['**/*.astro'],
+    files: ['/*.astro'],
     languageOptions: {
       parser: eslintPluginAstro.parser,
       parserOptions: {
@@ -35,5 +35,5 @@ export default tseslint.config(
         extraFileExtensions: ['.astro'],
       },
     },
-  }
-);
+  },
+];
